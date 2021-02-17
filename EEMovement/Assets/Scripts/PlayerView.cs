7fb9b9282b20e9355894c7bbd465621c13f21d4b
Twilit <kinlongsha@hotmail.co.uk>
2021-02-17 16:17:57 +0000

    Movement

---
+++ EEMovement/Assets/Scripts/PlayerView.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+
+    public GameObject currentVantage;
+    public GameObject camera;
+
+    public float timeToTravel = 0.5f;
+    public float timeToTurn = 0.5f;
+    bool travelling;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //MovementCheck();
+    }
+
+    public void AssignLocation(GameObject nextVantage)
+    {
+        if (!travelling)
+        {
+            currentVantage = nextVantage;
+
+            StartCoroutine(MoveToNextVantage(nextVantage.transform, nextVantage.GetComponent<VantagePoint>().cameraRot));
+        }
+    }
+
+    void MovementCheck()
+    {
+        if (currentVantage != null)
+        {
+            if (transform.position != currentVantage.transform.position)
+            {
+                travelling = true;
+            }
+        }
+    }
+
+    IEnumerator MoveToNextVantage(Transform targetPos, Quaternion targetRot)
+    {
+        travelling = true;
+        Transform currentPos = transform;
+        float t = 0f;
+
+        while (t < timeToTravel)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(currentPos.position, targetPos.position, Mathf.SmoothStep(0, timeToTravel, t));
+
+            yield return null;
+        }
+
+        //StartCoroutine(TurnCamera(targetRot));
+        camera.transform.rotation = targetRot;
+
+        travelling = false;
+    }
+
+    IEnumerator TurnCamera(Quaternion targetRot)
+    {
+        float t2 = 0f;
+        Quaternion currentRot = camera.transform.rotation;
+
+        while (t2 < timeToTurn)
+        {
+            t2 += Time.deltaTime;
+            camera.transform.rotation = Quaternion.Lerp(currentRot, targetRot, Mathf.SmoothStep(0, timeToTurn, t2));
+
+            yield return null;
+        }
+    }
+}
---
+++ EEMovement/Assets/Scripts/VantagePoint.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VantagePoint : MonoBehaviour
+{
+    public GameObject cameraPointer;
+    public Quaternion cameraRot;
+
+    GameObject player;
+    MeshRenderer arrowModel;
+    CapsuleCollider clickCollider;
+    bool currentlyHere = false;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        arrowModel = transform.GetChild(0).GetComponent<MeshRenderer>();
+        clickCollider = GetComponent<CapsuleCollider>();
+
+        cameraRot = cameraPointer.transform.rotation;
+        cameraPointer.SetActive(false);
+    }
+
+    void Update()
+    {
+        PlayerLocationCheck();
+        HandleArrow();
+    }
+
+    private void OnMouseDown()
+    {
+        print("clicked");
+        player.GetComponent<PlayerView>().AssignLocation(this.gameObject);
+    }
+
+    void PlayerLocationCheck()
+    {
+        if (player.GetComponent<PlayerView>().currentVantage == this.gameObject)
+        {
+            currentlyHere = true;
+        }
+        else
+        {
+            currentlyHere = false;
+        }
+    }
+
+    void HandleArrow()
+    {
+        if (currentlyHere)
+        {
+            arrowModel.enabled = false;
+            clickCollider.enabled = false;
+        }
+        else
+        {
+            arrowModel.enabled = true;
+            clickCollider.enabled = true;
+        }
+    }
+}
